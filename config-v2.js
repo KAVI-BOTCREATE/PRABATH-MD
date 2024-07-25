a071d99012b8bc406b104faad2e97c76aa1b7b2a
KAVI-BOTCREATE <kavindudilsharawirasiri@gmail.com>
2024-07-25 12:23:23 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
-const GITHUB_AUTH_TOKEN = "null"
-const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const GITHUB_AUTH_TOKEN = "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"
+const BOT_NUMBER = "94714732848"
+const SESSION_ID = "PRABATH-MD~gX9BhZrC#D78BQ5gFsn5nnaijXwGqO1jdbwa-I3lSFBGQiCdDC-o"
 
 
 module.exports = {
